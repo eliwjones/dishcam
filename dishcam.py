a4c6f13a702ea1a66fefdc399645a805c902ab27
eliwjones <eli.jones@gmail.com>
2025-06-12 23:55:31 -0700

    dishcam.py: remove unused cruft.

--- dishcam.py
+++ dishcam.py
@@ -4,7 +4,7 @@ from google.genai import types
 from .auth import email, password, key_id, api_key, gemini_api_key
 
 
-def ask_about_image(img_path: str, question: str) -> str:
+def ask_about_image(img_path: str, question: str):
     client = genai.Client(api_key=gemini_api_key)
 
     with open(img_path, "rb") as fh:
@@ -16,20 +16,3 @@ def ask_about_image(img_path: str, question: str) -> str:
 
     response = client.models.generate_content(model="gemini-1.5-flash", contents=contents)
     return response.text
-
-
-"""
-
-from auth import email, password, key_id, api_key
-import util.wyze as wz
-
-
-mac = 'D03F27511BDF'
-access_token, refresh_token, user_id = wz.login(email, password, key_id, api_key)
-
-events = wz.recent_events(access_token, refresh_token, user_id, mac, minutes=30)
-
-start_ms, end_ms = wz.kvs_times(events[0])
-wz.dash_mpd(access_token, device_id=mac, model="WYZE_CAKP2JFUS", start_ms=start_ms, end_ms=end_ms)
-
-"""
